850c50ff67f4a728c4736bc78a878b71fa9bcce5
ArsalanDanis <arsalan.jamia@gmail.com>
2024-12-05 15:49:58 +0530

    next project with ant design

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Add a rule to transpile Ant Design icons
+      config.module.rules.push({
+        test: /\.js$/,
+        include: [
+          /node_modules\/@ant-design\/icons/,
+          /node_modules\/@ant-design\/icons-svg/,
+        ],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["next/babel"],
+          },
+        },
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import 'antd/dist/reset.css'; // Import Ant Design's global styles
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
